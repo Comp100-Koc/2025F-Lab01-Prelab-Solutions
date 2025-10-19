0233191eae91c3b75ead24676e99097927f1767e
mervevural1 <havural24@ku.edu.tr>
2025-10-19 11:13:06 +0300

    Delete q5.py

--- q5.py
+++
@@ -1,9 +0,0 @@
-"""
-Implement a function named calculate_area, which takes three arguments: a, b, and c, which are the lengths of the three sides. 
-This function should return the area of the triangle.
-"""
-
-def calculate_area(a, b, c):
-  s = 0.5 * (a+b+c)
-  A = (s*(s-a)*(s-b)*(s-c))**0.5
-  return A
