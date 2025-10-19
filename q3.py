b9a9f0aaa9e060c5cc6e6227751644fb61888559
mervevural1 <havural24@ku.edu.tr>
2025-10-19 10:24:36 +0300

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,6 @@
+"""
+Implement a function named solve_quadratic which takes three arguments: a, b, and c, which are the coefficients of the quadratic equation. 
+This function should return the solution written in readME file. You can assume that the quadratic equation will always have a solution.
+"""
+
+def solve_quadratic(a, b, c):
