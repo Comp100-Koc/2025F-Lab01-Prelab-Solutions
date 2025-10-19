c927af45a95822e270852891d744014c57514b4a
mervevural1 <havural24@ku.edu.tr>
2025-10-19 10:25:39 +0300

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,6 @@
+"""
+Implement a function named calculate_distance, which takes four arguments: x1, y1, x2, and y2, representing the coordinates of two points (x1, y1) and (x2, y2). 
+This function should return the Euclidean distance between these two points.
+"""
+
+def calculate_distance(x1, y1, x2, y2):
