e0e59a84465504d5ee2a91d887474f3f76ff002e
mervevural1 <havural24@ku.edu.tr>
2025-10-19 11:21:55 +0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -6,5 +6,7 @@ convert_temperature takes two arguments: temp and unit, which are the temperatur
 def convert_temperature(temp, unit):
   if unit == 'F':
     C = (temp - 32) / 1.8
+    return C
   elif unit == 'C':
     F = (temp * 1.8) + 32
+    return F
