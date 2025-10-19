55c217c8bd29847f22cf61166f2d0566ecc7da0f
mervevural1 <havural24@ku.edu.tr>
2025-10-19 10:20:58 +0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,8 @@
+"""
+This function should calculate the mathematical function f(x) = (5x-3) / (2x+6).
+Implement a function named calculate_f, which takes x as an argument, and returns (5x-3) / (2x+6).
+"""
 
+def calculate_f(x):
+
+  
