5f118c92bd1d840e75f0e4af9490213096de26ec
mervevural1 <havural24@ku.edu.tr>
2025-10-19 10:23:04 +0300

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,6 @@
+"""
+Implement a function named to_celcius that converts a temperature from Fahrenheit to Celsius. 
+to_celsius takes one argument: f, which is the temperature in Fahrenheit and should return the temperature in Celsius.
+"""
+
+def to_celsius(f):
