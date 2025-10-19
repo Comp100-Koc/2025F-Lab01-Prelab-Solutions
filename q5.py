fcca8410d647995504dbde30ea46527f77e247cc
mervevural1 <havural24@ku.edu.tr>
2025-10-19 10:26:32 +0300

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,6 @@
+"""
+Implement a function named calculate_area, which takes three arguments: a, b, and c, which are the lengths of the three sides. 
+This function should return the area of the triangle.
+"""
+
+def calculate_area(a, b, c):
